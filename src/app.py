2d09817c014fbae03bdd078dea28db6890a94eed
Chijith Jerin Stalin <88652930+Jerin-004@users.noreply.github.com>
2025-11-06 16:51:35 +0530

    Trigger CI CD

--- src/app.py
+++ src/app.py
@@ -1,19 +1,23 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import joblib
 import numpy as np
 
+# Pydantic request model
+class PredictRequest(BaseModel):
+    features: list
+
 app = FastAPI()
 
-# Load model at server start
+# Load the trained model
 model = joblib.load("artifacts/model.joblib")
 
 @app.get("/")
 def home():
-    return {"message": "Rainfall Prediction Model is Live!"}
+    return {"message": "Rainfall Prediction API is running ✅"}
 
 @app.post("/predict")
-def predict(features: list):
-    # features must be in the same order as training columns
-    arr = np.array(features).reshape(1, -1)
+def predict(request: PredictRequest):
+    arr = np.array(request.features).reshape(1, -1)
     pred = model.predict(arr)[0]
     return {"rainfall_prediction": "yes" if pred == 1 else "no"}
