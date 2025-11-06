aebceadd380212bc9b05a0b6ffaac6595526b84a
Chijith Jerin Stalin <88652930+Jerin-004@users.noreply.github.com>
2025-11-06 17:46:47 +0530

    Trigger CI CD

--- src/app.py
+++ src/app.py
@@ -2,15 +2,21 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
 import numpy as np
+import pandas as pd
+import json
 
-# Pydantic request model
 class PredictRequest(BaseModel):
     features: list
 
 app = FastAPI()
 
-# Load the trained model
+# Load trained model
 model = joblib.load("artifacts/model.joblib")
+
+# Load feature names from metadata.json
+with open("artifacts/metadata.json") as f:
+    meta = json.load(f)
+feature_names = meta["features"]
 
 @app.get("/")
 def home():
@@ -18,7 +24,7 @@ def home():
 
 @app.post("/predict")
 def predict(request: PredictRequest):
-  
+    # Convert input to DataFrame with correct column names
     df = pd.DataFrame([request.features], columns=feature_names)
     pred = model.predict(df)[0]
     return {"rainfall_prediction": "yes" if pred == 1 else "no"}
